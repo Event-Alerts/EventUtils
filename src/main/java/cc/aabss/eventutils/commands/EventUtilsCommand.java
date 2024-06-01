5a3ccbe7f61e11b9a42108ff7cb457708b0fbbcc
aabssmc <lilmikeyninja203@gmail.com>
2024-05-31 21:25:22 -0700

    made EventUtilsCommand open config screen

--- src/main/java/cc/aabss/eventutils/commands/EventUtilsCommand.java
+++ src/main/java/cc/aabss/eventutils/commands/EventUtilsCommand.java
@@ -1,13 +1,10 @@
 package cc.aabss.eventutils.commands;
 
-import cc.aabss.eventutils.EventUtils;
+import cc.aabss.eventutils.config.EventUtil;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import java.lang.reflect.Field;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
@@ -16,76 +13,14 @@ public class EventUtilsCommand {
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(
                 ClientCommandManager.literal("eventutilsconfig")
-                        .then((ClientCommandManager.argument("key", StringArgumentType.word())
-                                .suggests((context, builder) -> builder
-                                        .suggest("default-famous-ip")
-                                        .suggest("auto-tp")
-                                        .suggest("discord-rpc")
-                                        .suggest("simple-queue-msg")
-                                        .suggest("update-checker")
-                                        .suggest("famous-event")
-                                        .suggest("potential-famous-event")
-                                        .suggest("money-event")
-                                        .suggest("partner-event")
-                                        .suggest("fun-event")
-                                        .suggest("housing-event")
-                                        .suggest("community-event")
-                                        .suggest("civilization-event")
-                                        .buildFuture())
-                                .then(ClientCommandManager.argument("value", StringArgumentType.greedyString())
-                                        .suggests((context, builder) -> {
-                                            String key = StringArgumentType.getString(context, "key");
-                                            SuggestionsBuilder builder1 = builder.restart();
-                                            if (!key.equalsIgnoreCase("default-famous-ip")) {
-                                                builder1
-                                                        .suggest("true")
-                                                        .suggest("false");
-                                            }
-                                            return builder1.buildFuture();
-                                        })
-                                        .executes((context) -> run(context.getSource().getPlayer(), context.getInput()))))
-                                .executes((context) -> run(context.getSource().getPlayer(), context.getInput()))));
+                                .executes((context) -> run(context.getSource().getClient())));
     }
 
-    public static int run(ClientPlayerEntity client, String command) {
-        String[] split = command.split(" ");
-        if (split.length != 1 && split.length != 2) {
-            String key = split[1];
-            String value = split[2];
-            Field valueField;
-            if (key.equalsIgnoreCase("default-famous-ip")) {
-                try {
-                    valueField = EventUtils.class.getDeclaredField(key.toUpperCase().replaceAll("-", "_"));
-                    valueField.setAccessible(true);
-                    valueField.set(new EventUtils(), value);
-                } catch (IllegalAccessException | NoSuchFieldException e) {
-                    throw new RuntimeException(e);
-                }
-
-                EventUtils.CONFIG.saveObject(key, value);
-            } else {
-                if (!value.equalsIgnoreCase("true") && !value.equalsIgnoreCase("false")) {
-                    client.sendMessage(Text.literal("Invalid boolean. (true/false)").formatted(Formatting.RED));
-                    return -1;
-                }
-
-                try {
-                    valueField = EventUtils.class.getDeclaredField(key.toUpperCase().replaceAll("-", "_"));
-                    valueField.setAccessible(true);
-                    valueField.set(new EventUtils(), Boolean.valueOf(value));
-                } catch (IllegalAccessException | NoSuchFieldException e) {
-                    throw new RuntimeException(e);
-                }
-
-                EventUtils.CONFIG.saveObject(key, Boolean.valueOf(value));
-            }
-
-            EventUtils.CONFIG.saveConfig(EventUtils.CONFIG.JSON);
-            client.sendMessage(Text.literal("Set " + key + " to " + value + ".").formatted(Formatting.GREEN));
-            return 1;
-        } else {
-            client.sendMessage(Text.literal("Usage: /" + split[0] + " <key> <value>").formatted(Formatting.RED));
-            return -1;
+    public static int run(MinecraftClient client) {
+        client.setScreen(EventUtil.screen(client.currentScreen));
+        if (client.player != null) {
+            client.player.sendMessage(Text.literal("Opening screen..").formatted(Formatting.GREEN));
         }
+        return 1;
     }
 }
--- src/main/java/cc/aabss/eventutils/config/EventUtil.java
+++ src/main/java/cc/aabss/eventutils/config/EventUtil.java
@@ -7,11 +7,15 @@ import cc.aabss.eventutils.commands.EventUtilsCommand;
 import cc.aabss.eventutils.commands.TestNotificationCommand;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
 import net.minecraft.client.network.ServerAddress;
@@ -29,6 +33,7 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
 
 import static cc.aabss.eventutils.EventUtils.*;
 import static cc.aabss.eventutils.EventUtils.TEXT;
@@ -244,5 +249,83 @@ public class EventUtil {
             }
         }
         return false;
+    }
+
+    public static Screen screen(Screen parent){
+        ConfigBuilder builder = ConfigBuilder.create()
+                .setParentScreen(parent)
+                .setTitle(Text.literal("Event Utils Mod Config"));
+        ConfigCategory generalCategory = builder.getOrCreateCategory(Text.literal("General"));
+        generalCategory.addEntry(ConfigEntryBuilder.create()
+                .startTextField(Text.literal("Default Famous IP"), EventUtils.DEFAULT_FAMOUS_IP)
+                .setDefaultValue(() -> EventUtils.DEFAULT_FAMOUS_IP)
+                .setTooltip(Text.literal("The default ip for if a [potential] famous event is pinged with no ip inputted."))
+                .setSaveConsumer(newValue -> {
+                    EventUtils.DEFAULT_FAMOUS_IP = newValue;
+                    CONFIG.saveObject("default-famous-ip", EventUtils.DEFAULT_FAMOUS_IP);
+                    CONFIG.saveConfig(CONFIG.JSON);
+                })
+                .build());
+        generalCategory.addEntry(ConfigEntryBuilder.create()
+                .startBooleanToggle(Text.literal("Auto Teleport"), EventUtils.AUTO_TP)
+                .setDefaultValue(() -> EventUtils.AUTO_TP)
+                .setTooltip(Text.literal("Whether you should be automatically teleported to the server where the event resides."))
+                .setSaveConsumer(newValue -> {
+                    EventUtils.AUTO_TP = newValue;
+                    CONFIG.saveObject("auto-tp", EventUtils.AUTO_TP);
+                    CONFIG.saveConfig(CONFIG.JSON);
+                })
+                .build());
+        generalCategory.addEntry(ConfigEntryBuilder.create()
+                .startBooleanToggle(Text.literal("Discord RPC"), EventUtils.DISCORD_RPC)
+                .setDefaultValue(() -> EventUtils.DISCORD_RPC)
+                .setTooltip(Text.literal("Whether the Discord rich presence should be shown."))
+                .setSaveConsumer(newValue -> {
+                    EventUtils.DISCORD_RPC = newValue;
+                    CONFIG.saveObject("discord-rpc", EventUtils.DISCORD_RPC);
+                    CONFIG.saveConfig(CONFIG.JSON);
+                    if (newValue)
+                        EventUtils.client.connect();
+                    else
+                        EventUtils.client.disconnect();
+
+                })
+                .build());
+        generalCategory.addEntry(ConfigEntryBuilder.create()
+                .startBooleanToggle(Text.literal("Simplified Queue Message"), EventUtils.SIMPLE_QUEUE_MSG)
+                .setDefaultValue(() -> EventUtils.SIMPLE_QUEUE_MSG)
+                .setTooltip(Text.literal("Whether the queue message for InvadedLands should be simplified."))
+                .setSaveConsumer(newValue -> {
+                    EventUtils.SIMPLE_QUEUE_MSG = newValue;
+                    CONFIG.saveObject("simple-queue-msg", EventUtils.SIMPLE_QUEUE_MSG);
+                    CONFIG.saveConfig(CONFIG.JSON);
+                })
+                .build());
+        ConfigCategory alertCategory = builder.getOrCreateCategory(Text.literal("Alerts"));
+        alertEntry(alertCategory, "Famous Events", EventUtils.FAMOUS_EVENT, newValue -> EventUtils.FAMOUS_EVENT = newValue);
+        alertEntry(alertCategory, "Potential Famous Events", EventUtils.POTENTIAL_FAMOUS_EVENT, newValue -> EventUtils.POTENTIAL_FAMOUS_EVENT = newValue);
+        alertEntry(alertCategory, "Money Events", EventUtils.MONEY_EVENT, newValue -> EventUtils.MONEY_EVENT = newValue);
+        alertEntry(alertCategory, "Partner Events", EventUtils.PARTNER_EVENT, newValue -> EventUtils.PARTNER_EVENT = newValue);
+        alertEntry(alertCategory, "Fun Events", EventUtils.FUN_EVENT, newValue -> EventUtils.FUN_EVENT = newValue);
+        alertEntry(alertCategory, "Housing Events", EventUtils.HOUSING_EVENT, newValue -> EventUtils.HOUSING_EVENT = newValue);
+        alertEntry(alertCategory, "Community Events", EventUtils.COMMUNITY_EVENT, newValue -> EventUtils.COMMUNITY_EVENT = newValue);
+        alertEntry(alertCategory, "Civilization Events", EventUtils.CIVILIZATION_EVENT, newValue -> EventUtils.CIVILIZATION_EVENT = newValue);
+        return builder.build();
+    }
+
+    private static void alertEntry(
+            ConfigCategory category,
+            String name,
+            Boolean booleanToggle,
+            Consumer<Boolean> consumer
+    ){
+        category.addEntry(ConfigEntryBuilder.create()
+                .startBooleanToggle(Text.literal(name), booleanToggle)
+                .setDefaultValue(() -> booleanToggle)
+                .setTooltip(Text.literal("Whether you should be alerted for "+name.toLowerCase()+"."))
+                .setSaveConsumer(consumer
+                        .andThen(newValue -> CONFIG.saveConfig(CONFIG.JSON))
+                        .andThen(newValue -> CONFIG.saveObject(name.replaceAll("s$", ""), newValue)))
+                .build());
     }
 }
