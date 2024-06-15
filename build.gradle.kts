718fbfb48ebe74f6a7d5c39c7e014d8919448979
srnyx <25808801+srnyx@users.noreply.github.com>
2024-06-15 19:15:50 -0400

    Remove MC version abstraction

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,14 +10,11 @@ plugins {
     id("xyz.srnyx.gradle-galaxy") version "1.1.3"
 }
 
-var minecraftVersion = "1.20.6"
 var modVersion = "1.4"
-setupJava("cc.aabss", "${minecraftVersion}-${modVersion}", "Alerting for Event Alerts Minecraft events", JavaVersion.VERSION_21, null)
+setupJava("cc.aabss", "1.20.6-${modVersion}", "Alerting for Event Alerts Minecraft events", JavaVersion.VERSION_21, null)
 
 // Replacements for fabric.mod.json
-addReplacementsTask(setOf("fabric.mod.json"), getDefaultReplacements() + mapOf(
-    "mc_version" to minecraftVersion,
-    "mod_version" to modVersion))
+addReplacementsTask(setOf("fabric.mod.json"), getDefaultReplacements() + mapOf("mod_version" to modVersion))
 
 // Repositories
 repository(Repository.MAVEN_CENTRAL, Repository.JITPACK)
@@ -25,7 +22,7 @@ repository("https://maven.shedaniel.me", "https://maven.fabricmc.net", "https://
 
 // Dependencies
 dependencies {
-    minecraft("com.mojang", "minecraft", minecraftVersion)
+    minecraft("com.mojang", "minecraft", "1.20.6")
     mappings("net.fabricmc:yarn:1.20.6+build.3:v2")
     modImplementation("net.fabricmc", "fabric-loader", "0.15.11")
     modImplementation("net.fabricmc.fabric-api", "fabric-api", "0.99.0+1.20.6")
