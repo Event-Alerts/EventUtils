dba5843a8b94e270970463348317f4c46298c8c3
aabssmc <aabssmc@users.noreply.github.com>
2024-08-10 14:59:28 -0700

    make confirm disconnect, disconnect immediately

--- src/main/java/cc/aabss/eventutils/mixin/ButtonWidgetMixin.java
+++ src/main/java/cc/aabss/eventutils/mixin/ButtonWidgetMixin.java
@@ -7,6 +7,8 @@ import net.minecraft.client.gui.screen.*;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.PressableWidget;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.realms.gui.screen.RealmsMainScreen;
 import net.minecraft.text.Text;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -31,10 +33,33 @@ public abstract class ButtonWidgetMixin extends PressableWidget {
         final Screen current = client.currentScreen;
         client.setScreen(new ConfirmScreen(yes -> {
             if (yes) {
-                client.disconnect(new MultiplayerScreen(new TitleScreen()));
+                disconnect();
                 return;
             }
             client.setScreen(current);
         }, Text.literal("Confirm Disconnect"), Text.literal("Are you sure you want to disconnect?")));
+    }
+
+    private void disconnect() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client == null) {
+            return;
+        }
+        boolean bl = client.isInSingleplayer();
+        ServerInfo serverInfo = MinecraftClient.getInstance().getCurrentServerEntry();
+        client.world.disconnect();
+        if (bl) {
+            client.disconnect(new MessageScreen(Text.translatable("menu.savingLevel")));
+        } else {
+            client.disconnect();
+        }
+        TitleScreen titleScreen = new TitleScreen();
+        if (bl) {
+            client.setScreen(titleScreen);
+        } else if (serverInfo != null && serverInfo.isRealm()) {
+            client.setScreen(new RealmsMainScreen(titleScreen));
+        } else {
+            client.setScreen(new MultiplayerScreen(titleScreen));
+        }
     }
 }
