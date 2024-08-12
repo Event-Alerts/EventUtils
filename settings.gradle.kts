9182037d41b65e53ab239ff94ec6cac145671ccb
srnyx <25808801+srnyx@users.noreply.github.com>
2024-08-11 20:20:52 -0400

    Add support for 1.20.3 and 1.20.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,16 @@ plugins { id("dev.kikugie.stonecutter") version "0.4.3" }
 extensions.configure<StonecutterSettings> {
     kotlinController = true
     centralScript = "build.gradle.kts"
-    shared { versions("1.21.1", "1.21", "1.20.6", "1.20.4", "1.20.2", "1.20.1") } // Make sure to update .github/workflows/publish.yml!
+    shared {
+        versions( // Make sure to update .github/workflows/publish.yml when changing versions!
+            "1.21.1",
+            "1.21",
+            "1.20.6",
+            "1.20.5",
+            "1.20.4",
+            "1.20.3",
+            "1.20.2",
+            "1.20.1")
+    }
     create(rootProject)
 }
