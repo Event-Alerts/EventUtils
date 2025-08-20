4299503acbaf4eb4e9f82aa031ce549884ea0681
Mia <mia@github.com>
2025-08-20 18:55:46 -0400

    update: consolidate confirm disconnect mixin & multiversion support

--- src/main/java/cc/aabss/eventutils/mixin/ButtonWidgetMixin.java
+++ src/main/java/cc/aabss/eventutils/mixin/ButtonWidgetMixin.java
@@ -4,15 +4,12 @@ import cc.aabss.eventutils.EventUtils;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.*;
-import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.PressableWidget;
-import net.minecraft.client.network.ServerInfo;
-import net.minecraft.client.realms.gui.screen.RealmsMainScreen;
 import net.minecraft.text.Text;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,6 +19,8 @@ import static net.minecraft.text.Text.translatable;
 
 @Mixin(ButtonWidget.class)
 public abstract class ButtonWidgetMixin extends PressableWidget {
+    @Shadow public abstract void onPress();
+
     public ButtonWidgetMixin(int i, int j, int k, int l, Text text) {
         super(i, j, k, l, text);
     }
@@ -36,36 +35,10 @@ public abstract class ButtonWidgetMixin extends PressableWidget {
         final Screen current = client.currentScreen;
         client.setScreen(new ConfirmScreen(yes -> {
             if (yes) {
-                disconnect();
+                this.onPress();
                 return;
             }
             client.setScreen(current);
         }, translatable("eventutils.confirm_disconnect.title"), translatable("eventutils.confirm_disconnect.message")));
-    }
-
-    @Unique
-    private void disconnect() {
-        final MinecraftClient client = MinecraftClient.getInstance();
-        if (client == null || client.world == null) return;
-        final TitleScreen titleScreen = new TitleScreen();
-        client.world.disconnect();
-
-        // Singleplayer
-        if (client.isInSingleplayer()) {
-            client.disconnect(new MessageScreen(translatable("menu.savingLevel")));
-            client.setScreen(titleScreen);
-            return;
-        }
-        client.disconnect();
-
-        // Realms
-        final ServerInfo serverInfo = MinecraftClient.getInstance().getCurrentServerEntry();
-        if (serverInfo != null && serverInfo.isRealm()) {
-            client.setScreen(new RealmsMainScreen(titleScreen));
-            return;
-        }
-
-        // Multiplayer
-        client.setScreen(new MultiplayerScreen(titleScreen));
     }
 }
