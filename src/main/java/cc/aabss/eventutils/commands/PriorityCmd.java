e23cb403ec9f7b3659c22777f115571482b89aa9
Mia <mia@github.com>
2025-08-21 07:25:54 -0400

    feature: redesign & modify prioritytop and priority command with locale keys

--- src/main/java/cc/aabss/eventutils/commands/PriorityCmd.java
+++ src/main/java/cc/aabss/eventutils/commands/PriorityCmd.java
@@ -1,12 +1,12 @@
 package cc.aabss.eventutils.commands;
 
+import cc.aabss.eventutils.EventUtils;
 import com.mojang.brigadier.context.CommandContext;
 
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
+import net.minecraft.text.*;
 import net.minecraft.util.Formatting;
 
 import org.jetbrains.annotations.NotNull;
@@ -16,14 +16,14 @@ import java.util.List;
 
 
 public class PriorityCmd {
-    private static final int PLAYERS_PER_PAGE = 17; // maybe can change later
+    private static final int PLAYERS_PER_PAGE = 10;
 
     public static void priority(@NotNull CommandContext<FabricClientCommandSource> context, String name) {
         final MinecraftClient client = context.getSource().getClient();
         client.send(() -> {
             // Get names
-            if (client.world == null) {
-                context.getSource().sendFeedback(Text.literal("Player not found!!").formatted(Formatting.RED));
+            if (client.world == null || client.player == null) {
+                context.getSource().sendFeedback(Text.translatable("eventutils.command.priority.noplayer"));
                 return;
             }
             final List<String> namesSorted = client.world.getPlayers().stream()
@@ -33,10 +33,15 @@ public class PriorityCmd {
 
             final String nameLower = name.toLowerCase();
             for (final String playerName : namesSorted) if (nameLower.equals(playerName.toLowerCase())) {
-                context.getSource().sendFeedback(Text.literal(name + " has pickup priority #" + (namesSorted.indexOf(name) + 1) + " (based on people around you)"));
+                if (playerName.equalsIgnoreCase(client.player.getName().getString())) {
+                    context.getSource().sendFeedback(Text.translatable("eventutils.command.priority.self", "§6#" + (namesSorted.indexOf(playerName) + 1)));
+                } else {
+                    context.getSource().sendFeedback(Text.translatable("eventutils.command.priority.player", playerName, "§6#" + (namesSorted.indexOf(playerName) + 1)));
+                }
+
                 return;
             }
-            context.getSource().sendFeedback(Text.literal("Player not found!!").formatted(Formatting.RED));
+            context.getSource().sendFeedback(Text.translatable("eventutils.command.priority.noplayer"));
         });
     }
 
@@ -46,19 +51,20 @@ public class PriorityCmd {
         client.send(() -> {
             // Get names
             if (client.world == null || client.player == null) {
-                source.sendFeedback(Text.literal("No players found!").formatted(Formatting.RED));
+                source.sendFeedback(Text.translatable("eventutils.command.prioritytop.emptypage"));
                 return;
             }
             final List<String> namesSorted = client.world.getPlayers().stream()
                     .sorted(Comparator.comparingInt(AbstractClientPlayerEntity::getId))
                     .map(player -> player.getName().getString())
+                    .filter(str -> !EventUtils.isNPC(str))
                     .toList();
 
             // Check page bounds
             final int totalPlayers = namesSorted.size();
             final int totalPages = (int) Math.ceil((double) totalPlayers / PLAYERS_PER_PAGE);
             if (page > totalPages || page < 1) {
-                source.sendFeedback(Text.literal("No page exists (limit: " + totalPages + ")!").formatted(Formatting.RED));
+                source.sendFeedback(Text.translatable("eventutils.command.prioritytop.notapage", "§f" + totalPages));
                 return;
             }
 
@@ -67,14 +73,75 @@ public class PriorityCmd {
             final int start = pageIndex * PLAYERS_PER_PAGE;
             final int end = Math.min(start + PLAYERS_PER_PAGE, totalPlayers);
             final String clientName = client.player.getName().getString().toLowerCase();
-            final MutableText text = Text.literal("\nPage " + page + " of " + totalPages + ":\n");
+            final MutableText text = Text.translatable("eventutils.command.prioritytop.page", "§6" + page, "§6" + totalPages);
             for (int i = start; i < end; i++) {
                 final String name = namesSorted.get(i);
-                final MutableText item = Text.literal((i + 1) + ". " + name + "\n");
-                if (name.toLowerCase().equals(clientName)) item.formatted(Formatting.YELLOW);
+                final boolean isLocalPlayer = name.equalsIgnoreCase(clientName);
+                final String boldModifier = isLocalPlayer ? "§l" : "";
+                Style colorModifier;
+                int placement = i + 1;
+
+                colorModifier = switch (placement) {
+                    case 1 -> Style.EMPTY.withColor(TextColor.fromRgb(0xFFEA5C));
+                    case 2 -> Style.EMPTY.withColor(TextColor.fromRgb(0xC0C0C0));
+                    case 3 -> Style.EMPTY.withColor(TextColor.fromRgb(0xA97142));
+                    default -> {
+                        if (isLocalPlayer) {
+                            yield Style.EMPTY.withColor(TextColor.fromFormatting(Formatting.YELLOW));
+                        }
+                        yield Style.EMPTY.withColor(TextColor.fromFormatting(Formatting.WHITE));
+                    }
+                };
+
+                final MutableText item = Text.literal(String.format("%s%d - %s\n", boldModifier, placement, isLocalPlayer ? name + " (You)" : name)).fillStyle(colorModifier);
                 text.append(item);
             }
             source.sendFeedback(text);
+
+            MutableText lastpage = Text.literal("");
+            MutableText nextpage = Text.literal("");
+
+            if (page > 1) {
+                lastpage = Text.translatable(
+                        "eventutils.command.prioritytop.lastpage",
+                        page - 1
+                ).setStyle(
+                        //? if <=1.21.4 {
+                        Style.EMPTY.withClickEvent(
+                                new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/eventutils prioritytop " + (page - 1))
+                        )
+                        //?} else {
+                        
+                        /*Style.EMPTY.withClickEvent(
+                                new ClickEvent.RunCommand("/eventutils prioritytop " + (page - 1))
+                        )
+                        
+                        *///?}
+                );
+            }
+
+            if (page + 1 <= totalPages) {
+                nextpage = Text.translatable(
+                        "eventutils.command.prioritytop.nextpage",
+                        page + 1
+                ).setStyle(
+                        //? if <=1.21.4 {
+                        Style.EMPTY.withClickEvent(
+                                new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/eventutils prioritytop " + (page + 1))
+                        )
+                        //?} else {
+                        
+                        /*Style.EMPTY.withClickEvent(
+                                new ClickEvent.RunCommand("/eventutils prioritytop " + (page + 1))
+                        )
+                        
+                        *///?}
+                );
+            }
+
+            if (!lastpage.getString().isEmpty() || !nextpage.getString().isEmpty()) {
+                source.sendFeedback(Text.translatable("eventutils.command.prioritytop.pagebutton", lastpage, nextpage));
+            }
         });
     }
 }
