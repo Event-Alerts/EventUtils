e02dd07fcb396a0fb85d464cab4387b5628cff67
srnyx <25808801+srnyx@users.noreply.github.com>
2026-07-16 21:37:50 -0400

    Update Shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("dev.kikugie.loom-back-compat")
     id("xyz.srnyx.gradle-galaxy") version "3.2.0"
     id("me.modmuss50.mod-publish-plugin") version "2.1.1"
-    id("com.gradleup.shadow") version "9.5.1"
+    id("com.gradleup.shadow") version "9.6.0"
     id("net.kyori.blossom") version "2.2.0"
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.4.1" // For Blossom
 }
