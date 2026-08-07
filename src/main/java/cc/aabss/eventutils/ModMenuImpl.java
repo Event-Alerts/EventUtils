25ac8a303271d703d9757c27cdafb02ab869dcae
srnyx <25808801+srnyx@users.noreply.github.com>
2026-08-07 17:58:28 -0400

    Remove Mod Menu attachModpackBadges
    This isn't a modpack...

--- src/main/java/cc/aabss/eventutils/ModMenuImpl.java
+++ src/main/java/cc/aabss/eventutils/ModMenuImpl.java
@@ -6,16 +6,9 @@ import com.terraformersmc.modmenu.api.ModMenuApi;
 import dev.kikugie.fletching_table.annotation.fabric.Entrypoint;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.function.Consumer;
-
 
 @Entrypoint("modmenu")
 public class ModMenuImpl implements ModMenuApi {
-    @Override
-    public void attachModpackBadges(@NotNull Consumer<String> consumer) {
-        consumer.accept("modmenu");
-    }
-
     @Override @NotNull
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return ConfigScreen::getConfigScreen;
